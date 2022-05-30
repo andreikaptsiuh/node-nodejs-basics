acfa4dd5c2b25afa961a58e21d4f4e864482b85f
Andrei <kaptsiuhandrei@gmail.com>
2022-05-30 23:31:57 +0300

    feat: implement create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,29 @@
+import path from "path";
+import { fileURLToPath } from 'url';
+import fs, { constants } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, "files", "fresh.txt");
+
+    const createFileWithContent = () => {
+        const fileContent = "I am fresh and young";
+
+        fs.writeFile(filePath, fileContent, (error) => {
+            if (error) {
+                throw error;
+            }
+        });
+    };
+
+    fs.access(filePath, constants.F_OK, (error) => {
+        if (error) {
+            createFileWithContent();
+        } else {
+            const message = "FS operation failed";
+            throw message;
+        }
+    });
 };
