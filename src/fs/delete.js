0e1278fbdc5b566fda52cdf9d471f56dda079e01
Andrei <kaptsiuhandrei@gmail.com>
2022-05-31 23:05:35 +0300

    feat: implement remove file function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,17 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { unlink } from "fs/promises";
+import { fsErrorHandler } from "../errorHandlers/fsErrorHandler.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const remove = async () => {
-    // Write your code here 
+    const fileForRemovePath = path.join(__dirname, "files", "fileToRemove.txt");
+
+    try {
+        await unlink(fileForRemovePath);
+    } catch {
+        fsErrorHandler();
+    }
 };
