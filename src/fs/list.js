bdf5bd7e264a2e678a6fae2584897e23cee2f066
Andrei <kaptsiuhandrei@gmail.com>
2022-05-31 23:16:56 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,18 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { readdir } from "fs/promises";
+import { fsErrorHandler } from "../errorHandlers/fsErrorHandler.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const list = async () => {
-    // Write your code here 
+    const redFilesDirPath = path.join(__dirname, "files");
+
+    try {
+        const files = await readdir(redFilesDirPath);
+        console.log(files);
+    } catch (error) {
+        fsErrorHandler();
+    }
 };
