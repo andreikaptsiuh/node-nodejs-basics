4497d4c8a0c214c5dd7db28c2c53962fa5d862cf
Andrei <kaptsiuhandrei@gmail.com>
2022-05-31 23:23:42 +0300

    feat: implement read file function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { readFile } from "fs/promises";
+import { fsErrorHandler } from "../errorHandlers/fsErrorHandler.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+    const fileForReadPath = path.join(__dirname, "files", "fileToRead.txt");
+
+    try {
+        const text = await readFile(fileForReadPath, "utf-8");
+        console.log(text);
+    } catch {
+        fsErrorHandler();
+    };
 };
