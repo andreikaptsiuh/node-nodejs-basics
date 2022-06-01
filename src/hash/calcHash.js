a6c155d521f303a46bb877ce128478896f5a861f
Andrei <kaptsiuhandrei@gmail.com>
2022-06-01 23:33:28 +0300

    feat: implement read stream

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const calculateHash = async () => {
-    const hash = createHash('sha256');
+    const hash = createHash("sha256");
 
     const fileForHashPath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
     const fileForHashData = await readFile(fileForHashPath);
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,13 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+    const readFilePath = path.join(__dirname, "files", "fileToRead.txt");
+    const readStream = fs.createReadStream(readFilePath);
+
+    readStream.pipe(process.stdout);
 };
