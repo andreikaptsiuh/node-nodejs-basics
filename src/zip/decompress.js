11b2f62c5fbc83b65eb844079cc04242d48f1168
Andrei <kaptsiuhandrei@gmail.com>
2022-06-02 15:20:06 +0300

    feat: implement compress and decompress functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,24 @@
+import path from "path";
+import fs, { constants } from "fs";
+import zlib from "zlib";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const decompress = async () => {
-    // Write your code here 
+    const fileToCompressPath = path.join(__dirname, "files", "archive.gz");
+    const compressedFilePath = path.join(__dirname, "files", "fileToCompress.txt");
+
+    fs.access(fileToCompressPath, constants.F_OK, (error) => {
+        if (error) {
+           throw new Error("File for decompress wasn't found");
+        } else {
+            const readStream = fs.createReadStream(fileToCompressPath);
+            const unzip = zlib.createUnzip();
+            const writeStream = fs.createWriteStream(compressedFilePath);
+
+            readStream.pipe(unzip).pipe(writeStream);
+        }
+    });
 };
