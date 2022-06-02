fa4f875ebf1db5ff29f54bbc6961b3f6b72f77db
Andrei <kaptsiuhandrei@gmail.com>
2022-06-02 14:40:16 +0300

    feat: implement transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,15 @@
+import { Transform } from "stream";
+
 export const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform({ 
+        transform(chunk, encoding, callback) {
+            const transformedText = chunk.toString().split("").reverse().join("");
+            callback(null, transformedText);
+        }
+    });
+
+    process.stdin.pipe(transformStream).pipe(process.stdout);
+
+    process.stdout.write("\x1b[33m Exit: Cntrl + C \x1b[0m");
+    process.stdout.write("\nEnter text which should be transformed: \n");
 };
